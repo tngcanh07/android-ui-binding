aa95c60b2cabda6c274bb80f04a99f4965d91dca
toannguyen <toannguyen@nhatvietgroup.com.vn>
2015-07-08 11:55:00 +0700

    change folder structures

---
+++ src/main/java/com/github/tongca/ui/binding/UIConfig.java
@@ -0,0 +1,5 @@
+package com.github.tongca.ui.binding;
+
+class UIConfig {
+	public static boolean DEBUG = false;
+}
---
+++ src/main/java/com/github/tongca/ui/binding/provider/IResourceProvider.java
@@ -0,0 +1,6 @@
+package com.github.tongca.ui.binding.provider;
+
+public interface IResourceProvider {
+	public int getIdentifierByName(String name);
+
+}
